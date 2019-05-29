3d9349735d5a4c5ed2ae7fcc87fd16a7fbc33007
mizuki <midukihimmel@gmail.com>
2019-05-29 16:42:12 +0900

    山札生成処理を実装 ドロー処理の予定を記述

--- blackJackGame.py
+++ blackJackGame.py
@@ -2,21 +2,6 @@
 # https://qiita.com/hirossyi73/items/cf8648c31898216312e5
 # Pythonの練習としてこの課題をやってみる
 
-print('Hello world')
-
-
-card_number = {1,2,3,4,5,6,7,8,9,10,11,12,13}
-
-card_mark = {"ハート","ダイア","スペード","クローバー"}
-
-#ここにナンバーとマークでトランプのセットを作る
-#機能拡張でトランプ追加したいから、頑張って追加できるように作る
-deck = {}
-
-print (card_number)
-
-print (card_mark)
-
 # 初回処理、プレイヤー2枚ドロー、ディーラー2枚ドロー（2枚目は非公開）
 # ターンの処理、(1)プレイヤーのドロー、(2)カードの確認、(3)ディーラーのドロー、(4)カードの確認
 # プレイヤーは引いた後に、次のカードを引くか選ぶ（y / n)
@@ -24,12 +9,44 @@ print (card_mark)
 # 22以上でバースト（敗北）
 # コールした場合21に近いほうが勝ち
 
-dict_sample = {
-    "key1" : "value1",
-    "key2" : "value2",
-    "key3" : "value3",
-}
-print (dict_sample)
+print("Hello world")
+
+# input()でインプットを呼び出し
+yesNo = input()
+print('y / n' + yesNo)
+
+# pythonのIF文は文字列の場合は==かinを使って比較する
+if yesNo in "y" or yesNo in "n":
+    print ("おーけー")
+else:
+    print ("ng")
+
+card_number = {1,2,3,4,5,6,7,8,9,10,11,12,13}
+
+card_mark = {"ハート","ダイア","スペード","クローバー"}
+
+#ここにナンバーとマークでトランプのセットを作る
+#機能拡張でトランプ追加したいから、頑張って追加できるように作る
+deck = []
+
+print(deck)
+
+for cMark in card_mark:
+    # print(cNumber)
+
+    for cNumber in card_number:
+
+        deck.append(str(cNumber) + ":::" + cMark)
+
+
+#ドローしたカードは配列から消す
+# del( index )を使って消せばOK
+
+#ドロー処理は配列の要素数を最大値とする乱数を生成して、インデックスのカードを取り出して、消去する
+# 山札枚数一定以下（ルール調べて）で山札を補充する
+print(deck)
+
+
 
 
 
