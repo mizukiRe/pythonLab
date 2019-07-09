3aac8e6afa409d736a85efbd30b29baefc731e08
mizuki <midukihimmel@gmail.com>
2019-07-09 18:14:42 +0900

    親要素のaタグhref要素を取得するように実験 忘れるので必ず復習

--- scrapingTest.py
+++ scrapingTest.py
@@ -25,15 +25,27 @@ except:
 # .stripped_strings を使えば、空白を除く事ができる
 # .parent を使えば、１つ上の親要素を取得できる
 # 属性は　.get("属性名") で取得する。
-# find_parents() で
+# find_parents() を使えば見つけた要素から上の要素を取得することができる。
 
-# ここで欲しい要素を抽出すれば良い
+# print(soup.get_text()・・・ページからタグを除去して全テキストを抽出
 
-get_text = soup.find_all(text = re.compile("カカオ"))
+# for link in soup.find_all('a'):・・・ページの<a>タグ内にあるURLを全て抽出
+#       print(link.get('href'))
+
+# **やりたいこと
+# 特定の文字列を取得して、そのリンクを取得したい
+
+# シカと含まれている要素をすべて取得
+get_text = soup.find_all(text = re.compile("シカ"))
 
 print(get_text)
 
 for GT in get_text:
-    print(GT.find_parents(true))  
 
+    # 文字列だけ取得してみる
+    # print(GT.find_parent('a').string)
 
+    # find_parent("a")で親要素のaを取得してhrefを取り出す
+    print(GT.find_parent('a').get("href") + "END")
+   
+# めっちゃ忘れてる…。家かえって復習しないと忘れる
